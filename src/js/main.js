a01fa0ec57f3e7b82fd4d3cd00eef3a4f42d92fa
pranya333 <kirill-pranya@mail.ru>
2023-01-14 11:37:23 +0300

    numToString

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
-//here will by JavaScript code ...
+const num = 4677767;
+ const numMap = {
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        0: 'zero'
+    }
+function numToString(num, numMap) {
+   const numArr = num.toString().split('').reduce((acc, el) => `${acc} ${numMap[el]}` , '');
+    return numArr;
+}
+
+console.log(numToString(num, numMap));
